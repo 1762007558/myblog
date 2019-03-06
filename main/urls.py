4661d0560367ffc67ab6b47eec9fb3da91c1b741
yaozhen <1762007558@qq.com>
2019-03-06 17:00:45 +0800

    添加部署内容

--- main/urls.py
+++ main/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .views import markdown_uploader
 
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
-]
+    path('martor/', include('martor.urls')),
+    path('api/uploader/', markdown_uploader, name='markdown_uploader_page'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- pages/models.py
+++ pages/models.py
@@ -13,6 +13,11 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = '文章'
+        app_label = 'pages'
+
 class Tag(models.Model):
     name = models.CharField('标签名', max_length=30)
     articles = models.ManyToManyField('Article', through='ArticleTags')
@@ -20,9 +25,19 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = '标签'
+        app_label = 'pages'
+
 class ArticleTags(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
 
     def __str__(self):
         return '{0}[{1}]'.format(self.article.title, self.tag.name)
+
+    class Meta:
+        verbose_name = '文章标签'
+        verbose_name_plural = '文章标签'
+        app_label = 'pages'
